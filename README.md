021a6a08ff169c87364cbe7bdfc9acf5133dbae3
Dimas-Mukhtar <95218465+Dimas-Mukhtar@users.noreply.github.com>
2023-09-18 22:31:33 +0700

    replacing the placeholder in overview.html and product.html into real data product from data.json and display it

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
-# basic node js routing api
+# node js routing api
 Binar Academy, live coding chapter 2
-https://localhost:8000
-- GET /text, readFile read-this.txt
-- GET /product, send json message
-- GET /api, readFile data.json
-- GET /overview, readFile overview.html
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require("fs")
 const http = require("http")
-const url = require("url")
+const urll = require("url")
 const path = require("path")
 
 //////////////////////////////////////////////////////////
@@ -36,33 +36,58 @@ const path = require("path")
 // })
 
 ////////////////////////////////////////////////////
+const replaceTemplate = (template, product)=>{
+    let output = template.replace(/{%ProductName%}/g, product.productName)
+    output = output.replace(/{%Image%}/g, product.image)
+    output = output.replace(/{%Quantity%}/g, product.quantity)
+    output = output.replace(/{%Price%}/g, product.price)
+    output = output.replace(/{%Nutrients%}/g, product.nutrients)
+    output = output.replace(/{%Description%}/g, product.description)
+    output = output.replace(/{%From%}/g, product.from)
+    output = output.replace(/{%id%}/g, product.id)
+
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, "not organic")
+    if(product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, "organic")
+
+    return output
+}
+const data = fs.readFileSync(path.join(__dirname, "dev-data", "data.json"), "utf-8")
+const dataObj = JSON.parse(data)
+
+const overviewPage = fs.readFileSync(`${__dirname}/templates/overview.html`, "utf-8")
+const productTemplate = fs.readFileSync(`${__dirname}/templates/product.html`, "utf-8")
+const productCardTemplate = fs.readFileSync(`${__dirname}/templates/template-card.html`, "utf-8")
 /// SERVER dengan HTTP
 const server = http.createServer((req, res)=>{
-    const url = req.url
-    if(url === "/text"){
-        fs.readFile("./txt/read-this.txt", "utf-8", function(err, data){
-            if(err){
-                console.log(err)
-            } else{
-                res.writeHead(200, {"Content-Type": "text/txt"})
-                res.write(data)
-                res.end()
-            }
-        })
-    } else if(url === "/product"){
+    const { pathname: url, query} = urll.parse(req.url, true)
+
+    // Hello Page
+    if(url === "/hello"){
+        res.end("Hello FSW 2")
+
+    // Product Page
+    } else if(url === "/productttt"){
         res.end(JSON.stringify({
             data: "Product"
         }))
+
+    // Simple Api
     } else if(url === "/api"){
-        const filePath = path.join(__dirname, "dev-data", "data.json")
-        const data = fs.readFileSync(filePath)
         res.writeHead(200, {"Content-Type": "application/json"})
         res.end(data)
+
+    // Overview Page
     } else if(url === "/overview"){
-        const filePath = path.join(__dirname, "templates", "overview.html")
-        const overview = fs.readFileSync(filePath)
         res.writeHead(200, {"Content-Type": "text/html"})
-        res.end(overview)
+
+        const productCardsHtml = dataObj.map(el => replaceTemplate(productCardTemplate, el))
+        const output = overviewPage.replace("{%ProductCard%}", productCardsHtml)
+        res.end(output)
+    } else if(url === "/product"){
+        res.writeHead(200, {"Content-Type": "text/html"})
+        const product = dataObj[query.id]
+        const output = replaceTemplate(productTemplate, product)
+        res.end(output)
     }
     else{
         res.writeHead(404, {"Content-Type": "text/html"})
